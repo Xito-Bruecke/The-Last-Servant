2df08e5b28fb8f3f1fca9c36746f16859782d11b
Xiito <celyan@stayfitandhappy.ch>
2024-10-28 15:06:43 +0100

    Game Loop Done

---
+++ src/main/java/main/GamePanel.java
@@ -0,0 +1,85 @@
+package main;
+
+import entity.Player;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.Dimension;
+
+public class GamePanel extends JPanel implements Runnable{
+
+    // Screen Settings
+    final int originalTileSize = 16; // 16x16 pixels
+    final int scale = 4;
+
+    public int tileSize = originalTileSize * scale; // 64x64 pixels
+    final int maxScreenCol = 20;
+    final int maxScreenRow = 16;
+    final int screenWidth = tileSize * maxScreenCol; // 1280 pixels
+    final int screenHeight = tileSize * maxScreenRow; // 1024 pixels
+
+    // FPS
+    int FPS = 60;
+
+    KeyHandler keyH = new KeyHandler();
+    Thread gameThread;
+    Player player = new Player(this, keyH);
+
+    // Set player's default position
+    int playerX = 100;
+    int playerY = 100;
+    int playerSpeed = 4;
+
+    public GamePanel() {
+        this.setPreferredSize(new Dimension(screenWidth, screenHeight));
+        this.setBackground(Color.BLACK);
+        this.setDoubleBuffered(true);
+        this.addKeyListener(keyH);
+        this.setFocusable(true);
+    }
+
+    public void startGameThread(){
+        gameThread = new Thread(this);
+        gameThread.start();
+    }
+
+    @Override
+    public void run() {
+
+        double drawInterval = 1000000000 / FPS; // 0.01666 seconds
+        double delta = 0;
+        long lastTime = System.nanoTime();
+        long currentTime;
+
+        // Game Loop
+        while(gameThread != null){
+            currentTime = System.nanoTime();
+            delta += (currentTime - lastTime) / drawInterval;
+            lastTime = currentTime;
+
+            if(delta >= 1){
+                // 1 UPDATE: Update information such as character positions
+                update();
+
+                // 2 DRAW: Draw the screen with the updated information
+                repaint();
+
+                delta--;
+            }
+        }
+    }
+
+    public void update(){
+        player.update();
+    }
+
+    public void paintComponent(Graphics g){
+        super.paintComponent(g);
+
+        Graphics2D g2 = (Graphics2D) g;
+
+        player.draw(g2);
+
+        g2.dispose();
+    }
+}
---
+++ src/main/java/main/main.java
@@ -0,0 +1,24 @@
+package main;
+
+import javax.swing.*;
+
+public class main {
+    public static void main(String[] args) {
+
+        // Building the JFrame for the game
+        JFrame window = new JFrame();
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        window.setResizable(false);
+        window.setTitle("The Last Servant");
+
+        GamePanel gamePanel = new GamePanel();
+        window.add(gamePanel);
+
+        window.pack();
+
+        window.setLocationRelativeTo(null);
+        window.setVisible(true);
+
+        gamePanel.startGameThread();
+    }
+}
